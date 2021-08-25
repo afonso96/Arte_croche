a94b4523a7e1f5aa98778de64a9cceb25f5ecc26
Afonso Fachinette <afonso2011fachineti@hotmail.com>
2021-08-25 09:50:43 -0300

    create app products and tests

---
+++ products/tests/test_models.py
@@ -0,0 +1,39 @@
+import pytest
+from pytest_django.asserts import assertQuerysetEqual
+
+from ..models import Product
+from .factories import ProductFactory
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCategoryModel:
+    def test___str__(self, category):
+        assert category.__str__() == category.name
+        assert str(category) == category.name
+
+    def test_get_absolute_url(self, category):
+        url = category.get_absolute_url()
+        assert url == f"/products/category/{category.slug}/"
+
+
+class TestProductModel:
+    def test___str__(self, product):
+        assert product.__str__() == product.name
+        assert str(product) == product.name
+
+    def test_get_absolute_url(self, product):
+        url = product.get_absolute_url()
+        assert url == f"/products/{product.slug}/"
+
+    def test_available_manager(self):
+        ProductFactory(is_available=True)
+        ProductFactory(is_available=False)
+
+        assert Product.objects.all().count() == 2
+        assert Product.available.all().count() == 1
+        assertQuerysetEqual(
+            Product.available.all(),
+            Product.objects.filter(is_available=True),
+            transform=lambda x: x,
+        )
